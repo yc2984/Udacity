48360cd8606acb32983999b184c8785f01f7de16
Yang Cao <yang.cao@energyworx.org>
2020-01-04 15:55:05 +0100

    removed unneeded testing lines, added some quetsions

--- show_me_the_data_structure/3_huffman_coding.py
+++ show_me_the_data_structure/3_huffman_coding.py
@@ -215,7 +215,6 @@ if __name__ == "__main__":
     codes = {}
 
     a_great_sentence = "The bird is the word"
-    a_great_sentence = "yaaang"
 
     print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
     print ("The content of the data is: {}\n".format(a_great_sentence))
