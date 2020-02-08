7a97b96a24cb9b184f1d14f27699d773c90e8b57
Yang Cao <yang.cao@energyworx.org>
2020-02-08 20:09:27 +0100

    add graph_dfs

---
+++ advanced_algorithms/graph_dfs.py
@@ -0,0 +1,64 @@
+class GraphNode(object):
+    def __init__(self, val):
+        self.value = val
+        self.children = []
+
+    def add_child(self, new_node):
+        self.children.append(new_node)
+
+    def remove_child(self, del_node):
+        if del_node in self.children:
+            self.children.remove(del_node)
+
+
+class Graph(object):
+    def __init__(self, node_list):
+        self.nodes = node_list
+
+    def add_edge(self, node1, node2):
+        if (node1 in self.nodes and node2 in self.nodes):
+            node1.add_child(node2)
+            node2.add_child(node1)
+
+    def remove_edge(self, node1, node2):
+        if (node1 in self.nodes and node2 in self.nodes):
+            node1.remove_child(node2)
+            node2.remove_child(node1)
+
+
+nodeG = GraphNode('G')
+nodeR = GraphNode('R')
+nodeA = GraphNode('A')
+nodeP = GraphNode('P')
+nodeH = GraphNode('H')
+nodeS = GraphNode('S')
+
+graph1 = Graph([nodeS,nodeH,nodeG,nodeP,nodeR,nodeA] )
+graph1.add_edge(nodeG,nodeR)
+graph1.add_edge(nodeA,nodeR)
+graph1.add_edge(nodeA,nodeG)
+graph1.add_edge(nodeR,nodeP)
+graph1.add_edge(nodeH,nodeG)
+graph1.add_edge(nodeH,nodeP)
+graph1.add_edge(nodeS,nodeR)
+
+"""Using what you know about DFS for trees, apply this to graphs. 
+Implement the dfs_search to return the GraphNode with the value search_value starting at the root_node."""
+
+
+def dfs_search(root_node, search_value):  # [YC] This I didn't figure out.. # TODO: Why do you need visited list??
+    visited = []
+    stack = [root_node]
+    while len(stack) > 0:
+        current = stack.pop()
+        visited.append(current)
+        if current.value == search_value:
+            return current
+        for child in current.children:
+            if child not in visited:
+                stack.append(child)
+
+
+assert nodeA == dfs_search(nodeS, 'A')
+assert nodeS == dfs_search(nodeP, 'S')
+assert nodeR == dfs_search(nodeH, 'R')
