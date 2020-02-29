506bd2bc42b46bf29858f86984ddf171695c20ca
Yang Cao <yang.cao@energyworx.org>
2020-02-29 15:54:48 +0100

    compare cost not indicator

--- route_planner/student_code.py
+++ route_planner/student_code.py
@@ -10,29 +10,32 @@ def cal_distance(M, node_1, node_2):
 
 def shortest_path(M, start, goal):
     print("shortest path called")
-    distance_library = {node: (math.inf, []) for node in M.intersections}
+    distance_library = {node: (math.inf, math.inf, []) for node in M.intersections}  # {node : (indicator, distance, path=[])
     indicator_start_node = 0 + cal_distance(M, start, goal)
-    distance_library[start] = [indicator_start_node, [start]]
+    distance_library[start] = indicator_start_node, 0, [start]
 
-    frontiers = [(indicator_start_node, (start, [start]))]  # List of tuple, this is the priority queue
+    frontiers = [(indicator_start_node, (start, [start], 0))]  # frontiers: (indicator, (node, path=[], distance))
 
-    while len(frontiers) > 0:  # TODO: think about this condition 
-        current_min_dis, (current_min_node, current_min_path) = heapq.heappop(frontiers)
-        if current_min_dis < distance_library[current_min_node][0]:
-            distance_library[current_min_node] = current_min_dis, current_min_path
+    while len(frontiers) > 0:
+        current_min_indicator, (current_min_node, current_min_path, current_min_distance) = heapq.heappop(frontiers)
+        if current_min_distance < distance_library[current_min_node][1]:
+            distance_library[current_min_node] = current_min_indicator, current_min_distance, current_min_path
 
         # Check all the neighbors, mark them as frontier and select the one with the least g + h as visited.
         for neighbor in M.roads[current_min_node]:
+
             straight_dis_to_goal = cal_distance(M, neighbor, goal)
             cost = cal_distance(M, current_min_node, neighbor)
+
+            new_distance = cost + current_min_distance
             indicator = straight_dis_to_goal + cost
-            if indicator < distance_library[neighbor][0]:
+            if new_distance < distance_library[neighbor][1]:
                 new_path = current_min_path + [neighbor]
 
-                distance_library[neighbor] = (indicator, new_path)
-                heapq.heappush(frontiers, (indicator, (neighbor, new_path)))
+                distance_library[neighbor] = (indicator, new_distance, new_path)
+                heapq.heappush(frontiers, (indicator, (neighbor, new_path, new_distance)))
 
-    return distance_library[goal][1]
+    return distance_library[goal][2]
 
 
 map_40 = load_map('map-40.pickle')
